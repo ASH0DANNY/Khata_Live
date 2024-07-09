28a2f73febf1e9618584dede60a5240a48be1961
ASH0DANNY <ash22714497@gmail.com>
2024-07-09 16:41:10 +0530

    .

--- src/pages/history.js
+++ src/pages/history.js
@@ -8,32 +8,29 @@ import Footer from "../components/footer";
 
 const HistoryPage = () => {
   const [muiAlert, setmuiAlert] = useState(false);
-  const [allHistory, setAllHistory] = useState([]);
-
-  useEffect(() => {
-    const fetchvendors = async () => {
-      const tran_history = await fetch(
-        "http://localhost:4000/api/khata/transaction/histories"
-      );
-
-      await tran_history
-        .json()
-        .then((response) => {
-          const response1 = response;
-          const data = response1.History;
-          setAllHistory(data);
-          console.log("Historydata:" + data);
-          console.log(
-            "data[0]" + data[0] + "  data[0].vendor_name" + data[0].vendor_name
-          );
-        })
-        .catch((error) => {
-          setmuiAlert(true);
-          console.log("error:" + error);
-        });
-    };
-    fetchvendors();
-  }, []);
+  const allHistory = [
+    {
+      vendor_name: "Ankita1",
+      trans_type: "credit",
+      trans_amount: 1000,
+      trans_desc: "Farewell Party",
+      date: "2024-07-05",
+    },
+    {
+      vendor_name: "Ankita1",
+      trans_type: "credit",
+      trans_amount: 300,
+      trans_desc: "Farewell Party",
+      date: "2024-07-05",
+    },
+    {
+      vendor_name: "Ankita3",
+      trans_type: "credit",
+      trans_amount: 320,
+      trans_desc: "Farewell Party",
+      date: "2024-07-05",
+    },
+  ];
 
   return (
     <>
